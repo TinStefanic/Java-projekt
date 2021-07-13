bcf0fea493f568a682ca4496a856b40fc5fca4c6
TinStefanic <tin.stefanic@gmail.com>
2021-07-13 18:15:09 +0200

    Graph klasa dorađena, dodani testovi.

--- NajkraciPutevi/src/NajkraciPutevi/Graph.java
+++ NajkraciPutevi/src/NajkraciPutevi/Graph.java
@@ -1,6 +1,8 @@
 package NajkraciPutevi;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.List;
 
 
 /**
@@ -14,13 +16,8 @@ public class Graph {
     private Integer[][] gMatrix;
     
     // Liste susjeda i odgovarajuće težine odgovarajućeg vrha.
-    private ArrayList<Integer>[] gNeighbour;
+    private ArrayList[] gNeighbour;
     private ArrayList<Integer>[] gWeight;
-    
-    // Je li graf dovršen, graf se smatra završenim nakon prvog
-    // poziva metode koja postavlja upit o bridovima i više se 
-    // ne može mijenjati nakon toga.
-    private boolean complete = false;
     
     /**
      * Konstruktor, stvara graf od n vrhova bez bridova.
@@ -30,6 +27,8 @@ public class Graph {
     public Graph(int n) {
         this.n = n;
         gMatrix = new Integer[n][n];
+        gNeighbour = new ArrayList[n];
+        gWeight = new ArrayList[n];
         for (int i = 0; i < n; ++i) {
             gNeighbour[i] = new ArrayList<Integer>();
             gWeight[i] = new ArrayList<Integer>();
@@ -38,22 +37,21 @@ public class Graph {
     
     /**
      * Dodaje novi brid u graf, tj. mijenja težinu brida
-     * ako brid već postoji u grafu. Baca iznimku ako je 
-     * metoda pozvana nakon što se graf više ne smije mijenjati.
+     * ako brid već postoji u grafu.
      * 
      * @param start početni vrh
      * @param end završni vrh.
      * @param w težina brida.
      */
-    public void addEdge(int start, int end, int w) throws Exception {
-        if (complete) throw new Exception("Graf se više ne može mijenjati.");
+    public void addEdge(int start, int end, int w) {
+        if (start == end) return; // Ne dozvoljavamo petlje.
         gMatrix[start][end] = w;
         int pos = gNeighbour[start].indexOf(end);
         if (pos == -1) { // Dodavanje novog brida.
             gNeighbour[start].add(end);
             gWeight[start].add(w);
         } else { // Promjena težine postojećeg brida.
-            gWeight[start].set(end, w);
+            gWeight[start].set(pos, w);
         }
     }
     
@@ -73,8 +71,7 @@ public class Graph {
      * @param end završni vrh
      * @return težina brida između dva vrha, ili null ako brid ne postoji
      */
-    public Integer weightBetween(int start, int end) {
-        complete = true;
+    public Integer getWeightBetween(int start, int end) {
         if (start < 0 || start >= n || end < 0 || end >= n) return null;
         else return gMatrix[start][end];
     }
@@ -87,8 +84,7 @@ public class Graph {
      * @return indeks susjeda na poziciji pos
      */
     public Integer getNeighbourAt(int start, int pos) {
-        complete = true;
-        return gNeighbour[start].get(pos);
+        return (Integer)gNeighbour[start].get(pos);
     }
     
     /**
@@ -99,8 +95,17 @@ public class Graph {
      * @return težina brida između vrha start i vrha na poziciji pos
      */
     public Integer getWeightAt(int start, int pos) {
-        complete = true;
-        return gWeight[start].get(pos);
+        return (Integer)gWeight[start].get(pos);
+    }
+    
+    /**
+     * Vraća brid između vrha start i vrha na poziciji pos u njegovoj listi susjeda.
+     * @param start početni vrh
+     * @param pos pozicija u listi susjeda
+     * @return brid između vrha start i vrha na poziciji pos
+     */
+    public Edge getEdgeAt(int start, int pos) {
+        return new Edge(start, getNeighbourAt(start, pos), getWeightAt(start, pos));
     }
     
     /**
@@ -110,16 +115,107 @@ public class Graph {
      * @return broj susjeda u listi susjeda od start
      */
     public int getNumNeighbours(int start) {
-        complete = true;
-        return gNeighbour[start].size();
+        return (Integer)gNeighbour[start].size();
     }
     
     /**
-     * Vraća je li faza konstrukcije grafa završila.
-     * 
-     * @return boolean je li graf gotov s konstrukcijom 
+     * Provjerava ima li graf negativne bridove.
      */
-    public boolean isComplete() {
-        return complete;
+    public boolean hasNegativeWeights() {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (gMatrix[i][j] != null && gMatrix[i][j] < 0)
+                    return true;
+            }
+        }
+        return false;
     }
+    /**
+     * Stvara random generirani graf s danim parametrima, bez negativnih bridova.
+     * 
+     * @param n broj vrhova u grafu
+     * @param directed je li graf usmjeren
+     * @param density koliko su bridovi česti u grafu, 1.0 znači da postoji
+     *                brid između svaka dva vrha
+     * @param maxWeight najveća težina u grafu, uključivo
+     * @return random generirani graf
+     */
+    public static Graph randomGraph(int n, boolean directed, double density, int maxWeight) {
+        Random rand = new Random();
+        Graph ret = new Graph(n);
+        for (int i = 0; i < n; ++i) {
+            for (int j = (directed) ? 0 : i+1; j < n; ++j) {
+                if (i == j) continue; // Ne dozvoljavamo petlje.
+                if (rand.nextDouble() < density) {
+                    int w = rand.nextInt(maxWeight)+1;
+                    ret.addEdge(j, i, w);
+                    if (!directed) ret.addEdge(i, j, w);
+                }
+            }
+        }
+        return ret;
+    }
+    
+    /**
+     * Stvara random generirani graf s danim parametrima, bez negativnih bridova.
+     * 
+     * @param n broj vrhova u grafu
+     * @param directed je li graf usmjeren
+     * @param density koliko su bridovi česti u grafu, 1.0 znači da postoji
+     *                brid između svaka dva vrha
+     * @return random generirani graf
+     */
+    public static Graph randomGraph(int n, boolean directed, double density) {
+        return randomGraph(n, directed, density, 10);
+    }
+    
+    /**
+     * Stvara random generirani graf s danim parametrima, bez negativnih bridova.
+     * 
+     * @param n broj vrhova u grafu
+     * @param directed je li graf usmjeren
+     * @return random generirani graf
+     */
+    public static Graph randomGraph(int n, boolean directed) {
+        return randomGraph(n, directed, 0.2);
+    }
+    
+    /**
+     * Stvara random generirani neusmjereni graf, bez negativnih bridova.
+     * 
+     * @param n broj vrhova u grafu
+     * @return random generirani graf
+     */
+    public static Graph randomGraph(int n) {
+        return randomGraph(n, false);
+    }
+    
+    /**
+     * Stvara random generirani neusmjereni graf s n vrhova, bez negativnih bridova.
+     * 
+     * @return random generirani graf
+     */
+    public static Graph randomGraph() {
+        return randomGraph(10);
+    }
+    
+    /**
+     * Vraća graf zapisan kao string.
+     */
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < n; ++i) {
+            ret += i + ".";
+            for (int j = 0; j < n; ++j) {
+                if (gMatrix[i][j] != null) {
+                    if (!ret.endsWith(".")) ret += ",";
+                    ret += " " + j;
+                    ret += ":" + gMatrix[i][j];
+                }
+            }
+            ret += "\n";
+        }
+        return ret;
+    } 
 }
