d2ac2bbe6d24737f41503a1f1c034796f188ed8f
marcihel05 <helena.marcius05@gmail.com>
2021-07-15 21:49:08 +0200

    shortest path draw

--- NajkraciPutevi/src/najkraciputevi/DrawGraph.java
+++ NajkraciPutevi/src/najkraciputevi/DrawGraph.java
@@ -6,11 +6,14 @@
 package najkraciputevi;
 
 
+
+
 import org.graphstream.graph.*;
 import org.graphstream.graph.implementations.*;
 /**
  * crtanje zadanog grafa (koristeći biblioteku graphstream)
  * @param g - graf koji se crta
+ * @source - https://graphstream-project.org/
  */
 public class DrawGraph {
     
@@ -20,26 +23,27 @@ public class DrawGraph {
     {
         this.g = g;
     }
-    public void draw() {
-		System.setProperty("org.graphstream.ui", "swing");
+    public void draw() 
+    {
+        System.setProperty("org.graphstream.ui", "swing");
 		
-		org.graphstream.graph.Graph graph = new SingleGraph("Graph");
-                for(int i = 0; i < g.getN(); ++i)
-                    graph.addNode(Integer.toString(i)).setAttribute("label", Integer.toString(i));
-		for(int i = 0; i < g.getN(); ++i){
-                    for( int j = i; j< g.getN(); ++j){
-                        var w = g.getWeightBetween(i,j);
-                        if( w != null){
-                            String is = Integer.toString(i);
-                            String js = Integer.toString(j);
-                            graph.addEdge(is+js, is, js).setAttribute("length", w);
-                        }
-                    }
+	org.graphstream.graph.Graph graph = new SingleGraph("Graph");
+        for(int i = 0; i < g.getN(); ++i)
+           graph.addNode(Integer.toString(i)).setAttribute("label", Integer.toString(i));
+	for(int i = 0; i < g.getN(); ++i){
+            for( int j = i; j< g.getN(); ++j){
+                var w = g.getWeightBetween(i,j);
+                if( w != null){
+                    String is = Integer.toString(i);
+                    String js = Integer.toString(j);
+                    graph.addEdge(is+js, is, js).setAttribute("length", w);
                 }
-                graph.nodes().forEach(n -> n.setAttribute("ui.style", "fill-color: lightblue; size: 20; text-size: 13;"));
-                graph.edges().forEach(e -> e.setAttribute("label", "" + (int) e.getNumber("length")));
-                graph.edges().forEach(e -> e.setAttribute("ui.style", "text-color: darkred; text-size: 13;"));
+            }
+        }
+        graph.nodes().forEach(n -> n.setAttribute("ui.style", "fill-color: lightblue; size: 20; text-size: 13;"));
+        graph.edges().forEach(e -> e.setAttribute("label", "" + (int) e.getNumber("length")));
+        graph.edges().forEach(e -> e.setAttribute("ui.style", "text-color: darkred; text-size: 13; fill-color: darkgrey; size:1.5;"));
                 
-		graph.display();
+        graph.display();
 	}
 }
---
+++ NajkraciPutevi/src/najkraciputevi/DrawShortestPath.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package najkraciputevi;
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+/**
+ *
+ * @param g - graf na kojem se crta najkraći put
+ * @param sp - najkraći pput
+ */
+public class DrawShortestPath {
+    
+    najkraciputevi.Graph g;
+    ShortestPath sp;
+    
+    public DrawShortestPath( najkraciputevi.Graph g, ShortestPath sp)
+    {
+        this.g = g;
+        this.sp = sp;
+    }
+    
+    public void draw()
+    {
+        System.setProperty("org.graphstream.ui", "swing");
+		
+	org.graphstream.graph.Graph graph = new SingleGraph("Graph");
+        for(int i = 0; i < g.getN(); ++i)
+           graph.addNode(Integer.toString(i)).setAttribute("label", Integer.toString(i));
+        graph.nodes().forEach(n -> n.setAttribute("ui.style", "fill-color: lightblue; size: 20; text-size: 13;"));
+        graph.getNode(Integer.toString(sp.getStart())).setAttribute("ui.style", "fill-color: lightgreen; size: 25; text-size: 13;");
+        graph.getNode(Integer.toString(sp.getEnd())).setAttribute("ui.style", "fill-color: lightgreen; size: 25; text-size: 13;");
+
+        for(Edge edge: sp.getEdges()){ //bridovi u najkracem putu
+            String s = Integer.toString(edge.getStart());
+            String e = Integer.toString(edge.getEnd());
+            int w = edge.getWeight();
+            graph.addEdge(s+e, s, e).setAttribute("length", w);
+        }
+        
+        graph.edges().forEach(e -> e.setAttribute("label", "" + (int) e.getNumber("length")));
+        graph.edges().forEach(e -> e.setAttribute("ui.style", "text-color: black; text-size: 14; fill-color: green; size: 2;"));
+        
+        for( int i = 0; i < g.getN(); ++i){
+            for( int j = 0; j< g.getN(); ++j){
+                var w = g.getWeightBetween(i,j);
+                if( w != null){
+                    String is = Integer.toString(i);
+                    String js = Integer.toString(j);
+                    Node n = graph.getNode(is);
+                    if( !n.hasEdgeToward(js)){
+                        graph.addEdge(is+js, is, js).setAttribute("length", w);
+                        org.graphstream.graph.Edge e = graph.getEdge(is+js);
+                        e.setAttribute("label", "" + (int) e.getNumber("length"));
+                        e.setAttribute("ui.style","text-color:darkred; text-size: 14; size:1.5; fill-color:darkgrey;" );
+                    }
+                }
+            }
+        }
+        graph.display();
+    }
+}
