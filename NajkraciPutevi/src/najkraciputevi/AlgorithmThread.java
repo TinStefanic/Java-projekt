a78a502ab8b6b1dc3098095fd9117fc1fbc790ee
Vinko Lovrenčić <winco@acer.home.com>
2021-07-16 00:57:51 +0200

    vinko 2 push gui

--- NajkraciPutevi/src/najkraciputevi/AlgorithmThread.java
+++ NajkraciPutevi/src/najkraciputevi/AlgorithmThread.java
@@ -18,37 +18,40 @@ public class AlgorithmThread extends SwingWorker<Integer, Void>{
     GraphAlgorithm alg;
     int start;
     int end;
+    javax.swing.JTextArea textbox;
+    long extime;
+    int exresult;
     
-    public AlgorithmThread( GraphAlgorithm Alg, Database d, int id, int s, int e)
+    
+    public AlgorithmThread( GraphAlgorithm Alg, Database d, int id, int s, int e, javax.swing.JTextArea tb)
     { 
         alg = Alg;
         graph_id = id;
         db = d;
         start = s;
         end = e;
+        textbox=tb;
     }
     
     @Override protected Integer doInBackground () throws Exception 
     {
-        int result = alg.query(start, end);
-        long time = alg.getLastTime();
-        db.insertCompletedAlgorithm( graph_id, alg.getName(), time, result, start, end );
-        return result;
+        exresult = alg.query(start, end);
+        extime = alg.getLastTime();
+        db.insertCompletedAlgorithm( graph_id, alg.getName(), extime, exresult, start, end );
+        return exresult;
     }
     
     protected void done()
     {
         try {
             int res = get();
+            System.out.println(extime);
+            System.out.println(exresult);
+            textbox.append(String.valueOf(extime)+"\r\n"+String.valueOf(exresult));
         } catch (InterruptedException ex) {
             Logger.getLogger(AlgorithmThread.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ExecutionException ex) {
             Logger.getLogger(AlgorithmThread.class.getName()).log(Level.SEVERE, null, ex);
         }
-    }
-    
-    @Override protected void process()
-    {
-        
     }
 }
